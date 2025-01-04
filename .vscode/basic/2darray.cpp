b1ddd13233cd769ffb6e2fbcdb0cc9291a231532
utsav-baghela <utsavu858@gmail.com>
2025-01-04 19:31:02 +0530

    printing 2d array in C language

--- .vscode/basic/2darray.cpp
+++ .vscode/basic/2darray.cpp
@@ -1,22 +1,51 @@
 //printing a 2d array
 
-#include <iostream>
-using namespace std;
+// in C++
 
-main(){
+// #include <iostream>
+// using namespace std;
+
+// main(){
+//     int arr[4][2] = {
+//         {10,11},
+//         {20, 21},
+//         {30 , 31},
+//         {40, 41},
+//     };
+//     int i, j;
+//     cout<<"Printing a 2D Array:/n";
+//     for(i=0; i<4 ; i++)
+//     {
+//         for(j=0;j<2;j++)
+//         {
+//             cout<<" "<<arr[i][j];
+//         }cout<<endl;
+//     }
+// }
+
+// IN C language
+
+#include <stdio.h>
+
+int main()
+{
     int arr[4][2] = {
-        {10,11},
+        {10, 11},
         {20, 21},
-        {30 , 31},
-        {40, 41},
-    };
+        {30, 31},
+        {40, 41}};
+
     int i, j;
-    cout<<"Printing a 2D Array:/n";
-    for(i=0; i<4 ; i++)
+
+    printf("Printing a 2D Array:\n");
+    for (i = 0; i < 4; i++)
     {
-        for(j=0;j<2;j++)
+        for (j = 0; j < 2; j++)
         {
-            cout<<" "<<arr[i][j];
-        }cout<<endl;
+            printf("\t%d", arr[i][j]);
+        }
+        printf("\n");
     }
+
+    return 0;
 }
