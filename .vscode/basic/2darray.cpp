2db048c5e42e7001dae3931fa0547343f699b64b
utsav-baghela <utsavu858@gmail.com>
2025-01-06 19:39:00 +0530

    one dimensional array syntax

--- .vscode/basic/2darray.cpp
+++ .vscode/basic/2darray.cpp
@@ -25,27 +25,35 @@
 
 // IN C language
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int main()
-{
-    int arr[4][2] = {
-        {10, 11},
-        {20, 21},
-        {30, 31},
-        {40, 41}};
+// int main()
+// {
+//     int arr[4][2] = {
+//         {10, 11},
+//         {20, 21},
+//         {30, 31},
+//         {40, 41}};
 
-    int i, j;
+//     int i, j;
 
-    printf("Printing a 2D Array:\n");
-    for (i = 0; i < 4; i++)
-    {
-        for (j = 0; j < 2; j++)
-        {
-            printf("\t%d", arr[i][j]);
-        }
-        printf("\n");
-    }
+//     printf("Printing a 2D Array:\n");
+//     for (i = 0; i < 4; i++)
+//     {
+//         for (j = 0; j < 2; j++)
+//         {
+//             printf("\t%d", arr[i][j]);
+//         }
+//         printf("\n");
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
+
+
+//one dimensional array 
+//how to declare 
+
+
+//syntsx: data_type name of the array[no. of elements];
+
