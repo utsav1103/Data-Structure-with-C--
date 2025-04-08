d67ba8b538b9194e866ceaf46f96c2e0f35e763d
utsav-baghela <utsavu858@gmail.com>
2025-04-08 14:24:13 +0530

    signed unsigned integer

--- Cprogramming/integer.c
+++ Cprogramming/integer.c
@@ -2,9 +2,14 @@
 #include <limits.h>
 
 int main(){
-    int var1 = INT_MIN;
-    int var2 = INT_MAX;
+    /*
+      int var1 = INT_MIN;
+      int var2 = INT_MAX;
 
-    printf("range of signed integer is from: %d to %d", var1, var2);
-    return 0;
+      printf("range of signed integer is from: %d to %d", var1, var2);
+      return 0;
+      */
+//example 
+     int var = 2345567343;
+     printf("%d", var);
 }
