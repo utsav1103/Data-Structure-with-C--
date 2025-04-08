b8a731d00d9e9c943154828d264a4a9316508b78
utsav-baghela <utsavu858@gmail.com>
2025-04-08 14:27:51 +0530

    char data type

---
+++ Cprogramming/datatype/char.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main (){
+    //we are getting '!' this in result because we are suing char and giving a value that hold this character
+    char var = 33;
+    printf("%c", var);
+    return 0;
+}
