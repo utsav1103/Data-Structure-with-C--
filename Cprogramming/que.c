265a94d260701fee093850f316cd5c869e79d9f5
utsav-baghela <utsavu858@gmail.com>
2025-04-11 14:07:15 +0530

    problem solving

---
+++ Cprogramming/que.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    printf("%d", printf("%s", "Hello world!"));
+    return 0;
+}
+
+// what's the output
+// %s is used to print string of characters and %d whille print the total no. of characters 
+
+// so the expected output is Hello world!12
+
