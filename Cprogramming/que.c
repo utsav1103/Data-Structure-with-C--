d0b63a80b1db4e4a132a471fa8e70fe8961293e7
utsav-baghela <utsavu858@gmail.com>
2025-04-11 14:12:00 +0530

    problem solving

--- Cprogramming/que.c
+++ Cprogramming/que.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 
-int main(){
-    printf("%d", printf("%s", "Hello world!"));
-    return 0;
-}
+//int main(){
+ //   printf("%d", printf("%s", "Hello world!"));
+ //   return 0;
+//}
 
 // what's the output
 // %s is used to print string of characters and %d whille print the total no. of characters 
 
 // so the expected output is Hello world!12
 
+
+int main() {
+    printf("%s\n", "Hello");
+    printf("%10s", "Hello");
+    return 0;
+}
+
+//the output is hello
+                      //hello
+// this is because we use %10s this create the diffrent of 10 spaces                      
