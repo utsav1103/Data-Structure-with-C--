d749792a1715b5b2937418fc5c4bb94e1adf8c63
utsav-baghela <utsavu858@gmail.com>
2025-04-14 15:15:51 +0530

    taking input with scanf

---
+++ Cprogramming/function/basic.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (){
+    int a , b;
+    printf("enter the number\n");
+    scanf("%d", &a);
+    printf("enter the number\n");
+    scanf("%d", &b);
+    printf("%d + %d = %d, hello world ", a , b,
+    a+b);
+    return 0;
+}
