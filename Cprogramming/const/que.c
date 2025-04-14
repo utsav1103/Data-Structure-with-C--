a8dcd72737cef304297937c8ec75436fd9282f70
utsav-baghela <utsavu858@gmail.com>
2025-04-14 15:03:42 +0530

    problem solving

---
+++ Cprogramming/const/que.c
@@ -0,0 +1,13 @@
+// what's the output
+#include <stdio.h>
+
+
+
+int main (){
+    int var = 052;
+    printf("%d", var);
+    return 0;
+
+    /// Ans 42 because it's an octal value a 0 is in front of valuse
+    //  here we will mulitply 5* 8^1 + 2* 8^0 = 42
+}
